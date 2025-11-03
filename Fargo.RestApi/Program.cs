4d2530c6e27a4efd2f94624e9a41f0ed0622debe
Igor Silvestrin <silvestrinigor@icloud.com>
2025-11-03 20:55:44 -0300

    adicao openapi

--- Fargo.RestApi/Program.cs
+++ Fargo.RestApi/Program.cs
@@ -5,11 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+builder.Services.AddOpenApi();
+
 builder.Services.AddProblemDetails();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
 
 app.MapFagoArticle();
 
