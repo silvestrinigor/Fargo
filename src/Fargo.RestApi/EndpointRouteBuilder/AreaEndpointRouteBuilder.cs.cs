dff3c6df4c3a082a5b9d49079a19ac8c467165be
Igor Silvestrin <silvestrinigor@icloud.com>
2025-11-12 22:40:07 -0300

    extention refact

--- src/Fargo.RestApi/EndpointRouteBuilder/AreaEndpointRouteBuilder.cs.cs
+++ src/Fargo.RestApi/EndpointRouteBuilder/AreaEndpointRouteBuilder.cs.cs
@@ -1,39 +1,43 @@
 ﻿using Fargo.Application.Contracts;
 using Fargo.Application.Dtos;
+using Fargo.HttpApi.EndpointRouteBuilder;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fargo.HttpApi.EndpointRouteBuilder
 {
     public static class AreaEndpointRouteBuilder
     {
-        public static void MapFargoArea(this IEndpointRouteBuilder builder)
+        extension(IEndpointRouteBuilder builder)
         {
-            builder.MapGet("/areas/{area}", async (Guid area, [FromServices] IAreaApplicationService areaApplicationService)
-                => await areaApplicationService.GetAreaAsync(area));
+            public void MapFargoArea()
+            {
+                builder.MapGet("/areas", async ([FromServices] IAreaApplicationService areaApplicationService)
+                    => await areaApplicationService.GetAreaAsync());
 
-            builder.MapGet("/areas", async ([FromServices] IAreaApplicationService areaApplicationService)
-                => await areaApplicationService.GetAreaAsync());
+                builder.MapGet("/areas/{area}", async (Guid area, [FromServices] IAreaApplicationService areaApplicationService)
+                    => await areaApplicationService.GetAreaAsync(area));
 
-            builder.MapGet("/areas/guids", async ([FromServices] IAreaApplicationService areaApplicationService)
-                => await areaApplicationService.GetAreaGuidsAsync());
+                builder.MapGet("/areas/guids", async ([FromServices] IAreaApplicationService areaApplicationService)
+                    => await areaApplicationService.GetAreaGuidsAsync());
 
-            builder.MapPost("/areas", async ([FromBody] EntityCreateDto areaCreateDto, [FromServices] IAreaApplicationService areaApplicationService)
-                => await areaApplicationService.CreateAreaAsync(areaCreateDto));
+                builder.MapGet("/areas/{area}/entities", async (Guid area, [FromServices] IAreaApplicationService areaApplicationService)
+                    => await areaApplicationService.GetAreaEntitiesAsync(area));
 
-            builder.MapPatch("/areas/{area}", async (Guid area, [FromBody] EntityUpdateDto areaUpdateDto, [FromServices] IAreaApplicationService areaApplicationService)
-                => await areaApplicationService.UpdateAreaAsync(area, areaUpdateDto));
+                builder.MapPost("/areas", async ([FromBody] EntityCreateDto areaCreateDto, [FromServices] IAreaApplicationService areaApplicationService)
+                    => await areaApplicationService.CreateAreaAsync(areaCreateDto));
 
-            builder.MapDelete("/areas/{area}", async (Guid area, [FromServices] IAreaApplicationService areaApplicationService)
-                => await areaApplicationService.DeleteAreaAsync(area));
+                builder.MapPatch("/areas/{area}", async (Guid area, [FromBody] EntityUpdateDto areaUpdateDto, [FromServices] IAreaApplicationService areaApplicationService)
+                    => await areaApplicationService.UpdateAreaAsync(area, areaUpdateDto));
 
-            builder.MapGet("/areas/{area}/entities", async (Guid area, [FromServices] IAreaApplicationService areaApplicationService)
-                => await areaApplicationService.GetAreaEntitiesAsync(area));
+                builder.MapPut("/areas/{area}/entities/{entity}", async (Guid area, Guid entity, [FromServices] IAreaApplicationService areaApplicationService)
+                    => await areaApplicationService.InsertEntityIntoAreaAsync(area, entity));
 
-            builder.MapPut("/areas/{area}/entities/{entity}", async (Guid area, Guid entity, [FromServices] IAreaApplicationService areaApplicationService)
-                => await areaApplicationService.InsertEntityIntoAreaAsync(area, entity));
-
-            builder.MapDelete("/areas/{area}/entities/{entity}", async (Guid area, Guid entity, [FromServices] IAreaApplicationService areaApplicationService)
-                => await areaApplicationService.RemoveEntityFromAreaAsync(area, entity));
+                builder.MapDelete("/areas/{area}", async (Guid area, [FromServices] IAreaApplicationService areaApplicationService)
+                    => await areaApplicationService.DeleteAreaAsync(area));
+                
+                builder.MapDelete("/areas/{area}/entities/{entity}", async (Guid area, Guid entity, [FromServices] IAreaApplicationService areaApplicationService)
+                    => await areaApplicationService.RemoveEntityFromAreaAsync(area, entity));
+            }
         }
     }
 }
