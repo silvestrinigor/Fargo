fe80ac1674565ecdafdc723ec5bfda796218ca45
Igor Silvestrin <silvestrinigor@icloud.com>
2026-01-03 20:36:19 -0300

    remove old unit tests

--- tests/Fargo.Domain.Tests/EntitiesTests/ArticleTests/ArticleTests.cs
+++ tests/Fargo.Domain.Tests/EntitiesTests/ArticleTests/ArticleTests.cs
@@ -1,39 +1,6 @@
-﻿using UnitsNet.NumberExtensions.NumberToDensity;
-using UnitsNet.NumberExtensions.NumberToLength;
-using UnitsNet.NumberExtensions.NumberToMass;
-using UnitsNet.NumberExtensions.NumberToVolume;
-
-namespace Fargo.Domain.Tests.EntitiesTests.ArticleTests
+﻿namespace Fargo.Domain.Tests.EntitiesTests.ArticleTests
 {
     public class ArticleTests
     {
-        [Fact]
-        public void CreateArticle_WithLengthWidthHeight_ShouldCalculateVolume()
-        {
-            var article = new Article
-            {
-                Length = 2.Meters(),
-                Width = 3.Meters(),
-                Height = 4.Meters()
-            };
-            var expectedVolume = 24.CubicMeters();
-
-            Assert.Equal(expectedVolume, article.Volume);
-        }
-
-        [Fact]
-        public void CreateArticle_WithMassAndVolume_ShouldCalculateDensity()
-        {
-            var article = new Article
-            {
-                Mass = 12.Kilograms(),
-                Length = 2.Meters(),
-                Width = 2.Meters(),
-                Height = 3.Meters()
-            };
-            var expectedDensity = 1.KilogramsPerCubicMeter();
-
-            Assert.Equal(expectedDensity, article.Density);
-        }
     }
 }
