db08c5c2ad288a6d2f5aba2d3c275bb5586d1822
Igor Silvestrin <silvestrinigor@icloud.com>
2026-04-26 11:49:48 -0300

    Dotnet format

--- src/Fargo.Infrastructure/Migrations/20260426143214_UserAndUserGroupNameidIsNotKeyAndCanBeChanged.cs
+++ src/Fargo.Infrastructure/Migrations/20260426143214_UserAndUserGroupNameidIsNotKeyAndCanBeChanged.cs
@@ -1,56 +1,55 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Fargo.Infrastructure.Migrations
+namespace Fargo.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class UserAndUserGroupNameidIsNotKeyAndCanBeChanged : Migration
 {
     /// <inheritdoc />
-    public partial class UserAndUserGroupNameidIsNotKeyAndCanBeChanged : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropUniqueConstraint(
-                name: "AK_Users_Nameid",
-                table: "Users");
+        migrationBuilder.DropUniqueConstraint(
+            name: "AK_Users_Nameid",
+            table: "Users");
 
-            migrationBuilder.DropUniqueConstraint(
-                name: "AK_UserGroups_Nameid",
-                table: "UserGroups");
+        migrationBuilder.DropUniqueConstraint(
+            name: "AK_UserGroups_Nameid",
+            table: "UserGroups");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Users_Nameid",
-                table: "Users",
-                column: "Nameid",
-                unique: true);
+        migrationBuilder.CreateIndex(
+            name: "IX_Users_Nameid",
+            table: "Users",
+            column: "Nameid",
+            unique: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_UserGroups_Nameid",
-                table: "UserGroups",
-                column: "Nameid",
-                unique: true);
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_UserGroups_Nameid",
+            table: "UserGroups",
+            column: "Nameid",
+            unique: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropIndex(
-                name: "IX_Users_Nameid",
-                table: "Users");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropIndex(
+            name: "IX_Users_Nameid",
+            table: "Users");
 
-            migrationBuilder.DropIndex(
-                name: "IX_UserGroups_Nameid",
-                table: "UserGroups");
+        migrationBuilder.DropIndex(
+            name: "IX_UserGroups_Nameid",
+            table: "UserGroups");
 
-            migrationBuilder.AddUniqueConstraint(
-                name: "AK_Users_Nameid",
-                table: "Users",
-                column: "Nameid");
+        migrationBuilder.AddUniqueConstraint(
+            name: "AK_Users_Nameid",
+            table: "Users",
+            column: "Nameid");
 
-            migrationBuilder.AddUniqueConstraint(
-                name: "AK_UserGroups_Nameid",
-                table: "UserGroups",
-                column: "Nameid");
-        }
+        migrationBuilder.AddUniqueConstraint(
+            name: "AK_UserGroups_Nameid",
+            table: "UserGroups",
+            column: "Nameid");
     }
 }
