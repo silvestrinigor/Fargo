a33a8a663eae53a70b14ddeb14e541e10adc64e3
Igor Silvestrin <igor.silvestrin@havan.com.br>
2026-04-30 11:26:52 -0300

    Dotnet format

--- src/Fargo.Infrastructure/Migrations/20260430141819_RefactArticleImages.cs
+++ src/Fargo.Infrastructure/Migrations/20260430141819_RefactArticleImages.cs
@@ -1,22 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Fargo.Infrastructure.Migrations
+namespace Fargo.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class RefactArticleImages : Migration
 {
     /// <inheritdoc />
-    public partial class RefactArticleImages : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
 
-        }
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
 
-        }
     }
 }
