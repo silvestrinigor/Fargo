53688a4c3b604eeb0d16ef6d09896f5bf0cff1e3
Igor Silvestrin <igor.silvestrin@havan.com.br>
2026-05-04 13:55:17 -0300

    Migrations

--- src/Fargo.Infrastructure/Migrations/20260504165505_UserRequirePasswordColumNotRequired.Designer.cs
+++ src/Fargo.Infrastructure/Migrations/20260504165505_UserRequirePasswordColumNotRequired.Designer.cs
@@ -4,6 +4,7 @@ using Fargo.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Fargo.Infrastructure.Migrations
 {
     [DbContext(typeof(FargoDbContext))]
-    partial class FargoWriteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260504165505_UserRequirePasswordColumNotRequired")]
+    partial class UserRequirePasswordColumNotRequired
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
