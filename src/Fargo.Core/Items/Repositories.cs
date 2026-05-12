01878ea12bd42797689b005829344dd433511815
Igor Silvestrin <igor.silvestrin@havan.com.br>
2026-05-12 16:19:31 -0300

    Items

---
+++ src/Fargo.Core/Items/Repositories.cs
@@ -0,0 +1,34 @@
+namespace Fargo.Core.Items;
+
+/// <summary>
+/// Defines the repository contract for managing <see cref="Item"/> entities.
+/// </summary>
+public interface IItemRepository
+{
+    /// <summary>
+    /// Gets an item by its unique identifier.
+    /// </summary>
+    Task<Item?> GetByGuid(
+        Guid entityGuid,
+        CancellationToken cancellationToken = default
+    );
+
+    /// <summary>
+    /// Retrieves the unique identifiers of all items contained under a given item.
+    /// </summary>
+    Task<IReadOnlyCollection<Guid>> GetContainerDescendantGuids(
+        Guid itemGuid,
+        bool includeRoot = true,
+        CancellationToken cancellationToken = default
+    );
+
+    /// <summary>
+    /// Adds a new item to the persistence context.
+    /// </summary>
+    void Add(Item item);
+
+    /// <summary>
+    /// Removes an item from the persistence context.
+    /// </summary>
+    void Remove(Item item);
+}
