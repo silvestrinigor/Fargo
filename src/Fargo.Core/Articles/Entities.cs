7bba137e0683cd8fe7229105b37559a793c49b20
Igor Silvestrin <igor.silvestrin@havan.com.br>
2026-05-12 16:13:10 -0300

    Articles

--- src/Fargo.Core/Articles/Entities.cs
+++ src/Fargo.Core/Articles/Entities.cs
@@ -6,8 +6,6 @@ using UnitsNet.NumberExtensions.NumberToScalar;
 
 namespace Fargo.Core.Articles;
 
-#region Entity
-
 /// <summary>
 /// Represents an article in the system.
 /// </summary>
@@ -273,6 +271,8 @@ public class Article : ModifiedEntity, IPartitionedEntity, IActivable
     #endregion Partition
 }
 
+#region Container
+
 /// <summary>
 /// Defines container constraints for an <see cref="Article"/>.
 /// </summary>
@@ -309,6 +309,9 @@ public sealed class ArticleContainer
     }
 }
 
+#endregion Container
+
+#region Variation
 
 /// <summary>
 /// Defines an article variation relationship.
@@ -337,6 +340,10 @@ public sealed class ArticleVariation
         }
     }
 }
+
+#endregion Variation
+
+#region Pack
 
 /// <summary>
 /// Defines an article pack relationship.
@@ -392,6 +399,10 @@ public sealed class ArticlePack
     }
 }
 
+#endregion Pack
+
+#region Kit
+
 /// <summary>
 /// Defines an article kit relationship.
 /// </summary>
@@ -433,111 +444,4 @@ public sealed class ArticleKit
     }
 }
 
-#endregion Entity
-
-#region Repositories
-
-/// <summary>
-/// Defines the repository contract for managing <see cref="Article"/> entities.
-/// </summary>
-public interface IArticleRepository
-{
-    /// <summary>
-    /// Gets an article by its unique identifier.
-    /// </summary>
-    Task<Article?> GetByGuid(
-        Guid entityGuid,
-        CancellationToken cancellationToken = default
-    );
-
-    Task<bool> ExistsByBarcode(
-        Ean13 code
-    );
-
-    Task<bool> ExistsByBarcode(
-        Ean8 code
-    );
-
-    Task<bool> ExistsByBarcode(
-        UpcE code
-    );
-
-    Task<bool> ExistsByBarcode(
-        UpcA code
-    );
-
-    Task<bool> ExistsByBarcode(
-        Code128 code
-    );
-
-
-    Task<bool> ExistsByBarcode(
-        Code39 code
-    );
-
-    Task<bool> ExistsByBarcode(
-        Itf14 code
-    );
-
-    Task<bool> ExistsByBarcode(
-        Gs1128 code
-    );
-
-    Task<bool> ExistsByBarcode(
-        QrCode code
-    );
-
-    Task<bool> ExistsByBarcode(
-        DataMatrix code
-    );
-
-    /// <summary>
-    /// Determines whether the specified article has any associated items.
-    /// </summary>
-    Task<bool> HasItemsAssociated(
-        Guid articleGuid,
-        CancellationToken cancellationToken = default
-    );
-
-    /// <summary>
-    /// Adds a new article to the persistence context.
-    /// </summary>
-    void Add(Article article);
-
-    /// <summary>
-    /// Removes an article from the persistence context.
-    /// </summary>
-    void Remove(Article article);
-}
-
-#endregion Repositories
-
-#region Exceptions
-
-/// <summary>
-/// Exception thrown when an attempt is made to delete an article
-/// that still has items associated with it.
-/// </summary>
-public sealed class ArticleDeleteWithItemsAssociatedFargoDomainException(Guid articleGuid)
-    : FargoDomainException($"Article '{articleGuid}' cannot be deleted because it has associated items.")
-{
-    /// <summary>
-    /// Gets the identifier of the article that could not be deleted.
-    /// </summary>
-    public Guid ArticleGuid { get; } = articleGuid;
-}
-
-/// <summary>
-/// Exception thrown when a barcode is already assigned to a different article.
-/// </summary>
-public class ArticleBarcodeAlreadyInUseFargoDomainException(BarcodeFormat format, string code)
-    : FargoDomainException($"Barcode '{code}' ({format}) is already assigned to another article.")
-{
-    /// <summary>Gets the barcode format that conflicts.</summary>
-    public BarcodeFormat Format { get; } = format;
-
-    /// <summary>Gets the barcode code that conflicts.</summary>
-    public string Code { get; } = code;
-}
-
-#endregion Exceptions
+#endregion Kit
