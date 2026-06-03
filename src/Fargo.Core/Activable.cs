31d3bd9e65e5f6447d2f52cc395d60874590fdfd
Igor Silvestrin <silvestrinigor@icloud.com>
2026-06-03 19:14:30 -0300

    Removed activable and entity inferfaces from core shared

--- src/Fargo.Core/Activable.cs
+++ src/Fargo.Core/Activable.cs
@@ -1,6 +1,19 @@
-using Fargo.Core.Shared;
-
 namespace Fargo.Core;
+
+/// <summary>
+/// Represents an entity that can be activated or deactivated.
+/// </summary>
+/// <remarks>
+/// Activable entities can participate in business rules that require the entity
+/// to be active before an operation is allowed.
+/// </remarks>
+public interface IActivable : IEntity
+{
+    /// <summary>
+    /// Gets a value indicating whether the entity is currently active.
+    /// </summary>
+    bool IsActive { get; }
+}
 
 /// <summary>
 /// Thrown when an operation requires an entity to be active,
