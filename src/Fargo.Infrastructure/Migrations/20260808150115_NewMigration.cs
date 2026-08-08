3b7863297544a7ada2e1310a1068c3c1761ddad4
Igor da Silva Silvestrin <silvestrinigor@icloud.com>
2026-08-08 12:01:37 -0300

    Migration

---
+++ src/Fargo.Infrastructure/Migrations/20260808150115_NewMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fargo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "replaced_by_token_hash",
+                table: "refresh_tokens");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "replaced_by_token_hash",
+                table: "refresh_tokens",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: true);
+        }
+    }
+}
