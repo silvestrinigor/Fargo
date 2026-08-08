1bbf639bd584695913d2e6f54a823333cb8afb87
Igor da Silva Silvestrin <silvestrinigor@icloud.com>
2026-08-08 12:04:49 -0300

    Dotnet format

--- src/Fargo.Infrastructure/Migrations/20260808150115_NewMigration.cs
+++ src/Fargo.Infrastructure/Migrations/20260808150115_NewMigration.cs
@@ -1,29 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Fargo.Infrastructure.Migrations
+namespace Fargo.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class NewMigration : Migration
 {
     /// <inheritdoc />
-    public partial class NewMigration : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "replaced_by_token_hash",
-                table: "refresh_tokens");
-        }
+        migrationBuilder.DropColumn(
+            name: "replaced_by_token_hash",
+            table: "refresh_tokens");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "replaced_by_token_hash",
-                table: "refresh_tokens",
-                type: "character varying(512)",
-                maxLength: 512,
-                nullable: true);
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<string>(
+            name: "replaced_by_token_hash",
+            table: "refresh_tokens",
+            type: "character varying(512)",
+            maxLength: 512,
+            nullable: true);
     }
 }
