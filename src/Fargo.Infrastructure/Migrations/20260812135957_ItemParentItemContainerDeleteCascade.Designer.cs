d3ac514896c822b3d1870309c5369819f097d882
Igor Silvestrin <igor.silvestrin@havan.com.br>
2026-08-12 11:00:13 -0300

    When a container is deleted all items inside are deleted too.

--- src/Fargo.Infrastructure/Migrations/20260812135957_ItemParentItemContainerDeleteCascade.Designer.cs
+++ src/Fargo.Infrastructure/Migrations/20260812135957_ItemParentItemContainerDeleteCascade.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Fargo.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NpgsqlTypes;
@@ -12,9 +13,11 @@ using NpgsqlTypes;
 namespace Fargo.Infrastructure.Migrations
 {
     [DbContext(typeof(FargoDbContext))]
-    partial class FargoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260812135957_ItemParentItemContainerDeleteCascade")]
+    partial class ItemParentItemContainerDeleteCascade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
