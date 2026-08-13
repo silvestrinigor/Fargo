34e272873375836c09bb052389043ab561fc9e4f
Igor da Silva Silvestrin <silvestrinigor@icloud.com>
2026-08-13 20:50:59 -0300

    Doc

--- src/Fargo.Core/Audits/IAuditLogRepository.cs
+++ src/Fargo.Core/Audits/IAuditLogRepository.cs
@@ -1,6 +1,14 @@
 namespace Fargo.Core.Audits;
 
+/// <summary>
+/// Defines the repository contract for managing <see cref="AuditLog"/> entities.
+/// /// </summary>
 public interface IAuditLogRepository
 {
+    /// Adds an audit log to the repository.
+    /// </summary>
+    /// <param name="auditLog">
+    /// The audit log to add.
+    /// /// </param>
     void Add(AuditLog auditLog);
 }
