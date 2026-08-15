395e414d6a7c9cba6d82ea5d62d0292665cceb29
Igor da Silva Silvestrin <silvestrinigor@icloud.com>
2026-08-15 15:06:01 -0300

    Doc

--- src/Fargo.Application/Users/IUserQueryRepository.cs
+++ src/Fargo.Application/Users/IUserQueryRepository.cs
@@ -2,14 +2,53 @@ using Fargo.Application.Common;
 
 namespace Fargo.Application.Users;
 
+/// <summary>
+/// Defines read-only queries for user information.
+/// </summary>
 public interface IUserQueryRepository
 {
+    /// <summary>
+    /// Gets user information by its unique identifier.
+    /// </summary>
+    /// <param name="entityGuid">
+    /// The unique identifier of the user.
+    /// </param>
+    /// <param name="childOfAnyOfThesePartitions">
+    /// An optional collection of partition identifiers. When specified,
+    /// only users associated with a partition that is a child of any of
+    /// the specified partitions are considered.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token used to cancel the asynchronous operation.
+    /// </param>
+    /// <returns>
+    /// The user information if a matching user is found; otherwise,
+    /// <see langword="null"/>.
+    /// </returns>
     Task<UserDto?> GetInfoByGuidAsync(
         Guid entityGuid,
         IReadOnlyCollection<Guid>? childOfAnyOfThesePartitions = null,
         CancellationToken cancellationToken = default
     );
 
+    /// <summary>
+    /// Gets a paginated collection of user information.
+    /// </summary>
+    /// <param name="pagination">
+    /// The pagination parameters used to determine the requested page
+    /// and the number of users to return.
+    /// </param>
+    /// <param name="childOfAnyOfThesePartitions">
+    /// An optional collection of partition identifiers. When specified,
+    /// only users associated with a partition that is a child of any of
+    /// the specified partitions are considered.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// A token used to cancel the asynchronous operation.
+    /// </param>
+    /// <returns>
+    /// A read-only collection containing the requested page of user information.
+    /// </returns>
     Task<IReadOnlyCollection<UserDto>> GetManyInfoAsync(
         Pagination pagination,
         IReadOnlyCollection<Guid>? childOfAnyOfThesePartitions = null,
--- src/Fargo.Application/Users/UserAuthenticationCreateDto.cs
+++ src/Fargo.Application/Users/UserAuthenticationCreateDto.cs
@@ -2,6 +2,15 @@ using Fargo.Core.Security;
 
 namespace Fargo.Application.Users;
 
+/// <summary>
+/// Represents the data required to create authentication information for a user.
+/// </summary>
+/// <param name="Password">
+/// The initial password for the user's authentication credentials.
+/// </param>
+/// <param name="DefaultPasswordExpirationPeriod">
+/// The period after which the user's password expires.
+/// </param>
 public sealed record UserAuthenticationCreateDto(
     Password? Password = null,
     TimeSpan? DefaultPasswordExpirationPeriod = null
