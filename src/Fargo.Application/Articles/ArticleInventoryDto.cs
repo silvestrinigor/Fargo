5ce7862646e0eef5d7b9db67bde5fb54c0dd9547
Igor da Silva Silvestrin <silvestrinigor@icloud.com>
2026-08-30 13:37:54 -0300

    doc

--- src/Fargo.Application/Articles/ArticleInventoryDto.cs
+++ src/Fargo.Application/Articles/ArticleInventoryDto.cs
@@ -1,5 +1,11 @@
 namespace Fargo.Application.Articles;
 
+/// <summary>
+/// Represents a summary of inventory information for a specific article.
+/// </summary>
+/// <param name="TotalCount">
+/// The total number of items available in inventory for this article.
+/// </param>
 public sealed record ArticleInventoryDto(
     int TotalCount
 );
